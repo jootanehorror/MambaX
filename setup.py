ec23ce1d3cb020c053d6f44813f5eb750257e4d5
Oleg Kufa <103970725+jootanehorror@users.noreply.github.com>
2025-06-18 00:13:56 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="mambax",
-    version="0.1.0",  
+    version="0.2.0",  
     author="Oleg Kufa",
     author_email="os.schischkin@gmail.com",
     description="Optimized Mamba implementation with chunk processing and ONNX export",
